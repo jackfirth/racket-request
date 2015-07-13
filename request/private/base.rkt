9ffe5f186d0b685ec37f559c5460f7ddefdf14b4
Jack Firth <jackhfirth@gmail.com>
2015-07-13 01:21:25 -0700

    Rename requester

--- request/private/base.rkt
+++ request/private/base.rkt
@@ -5,23 +5,23 @@
          "call-response.rkt"
          "struct.rkt")
 
-(provide base-requester)
+(provide http-requester)
 
 
-(define (base-get url #:headers [headers '()])
+(define (http-get url #:headers [headers '()])
   (call-response/input-url url (get-impure-port _ headers)))
 
-(define (base-put url body #:headers [headers '()])
+(define (http-put url body #:headers [headers '()])
   (call-response/input-url url (put-impure-port _ body headers)))
 
-(define (base-post url body #:headers [headers '()])
+(define (http-post url body #:headers [headers '()])
   (call-response/input-url url (post-impure-port _ body headers)))
 
-(define (base-delete url #:headers [headers '()])
+(define (http-delete url #:headers [headers '()])
   (call-response/input-url url (delete-impure-port _ headers)))
 
-(define base-requester
-  (requester base-get
-             base-put
-             base-post
-             base-delete))
+(define http-requester
+  (requester http-get
+             http-put
+             http-post
+             http-delete))
