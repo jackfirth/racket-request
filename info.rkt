c641b1b9e6d553fc9d1eae258d5c7757359adb08
Jack Firth <jackhfirth@gmail.com>
2015-07-12 23:13:52 -0700

    Add package info

---
+++ info.rkt
@@ -0,0 +1,19 @@
+#lang info
+
+
+(define collection 'multi)
+
+
+(define version "0.1")
+
+
+(define deps
+  '("base"
+    "rackunit-lib"))
+
+
+(define build-deps
+  '("cover"
+    "rackunit-lib"
+    "racket-doc"
+    "doc-coverage"))
