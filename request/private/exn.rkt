029f54a8f781ea6dc518e426e42d1f2c430a1a71
Jack Firth <jackhfirth@gmail.com>
2015-07-14 00:40:15 -0700

    Add http-exception requester wrapping

---
+++ request/private/exn.rkt
@@ -0,0 +1,59 @@
+#lang racket
+
+(require fancy-app
+         "struct.rkt"
+         "wrap.rkt"
+         "call-response.rkt")
+
+(provide (struct-out exn:fail:network:http:code)
+         requester-http-exn
+         http-exn-of-code?)
+
+
+(define message-codes
+  (hash 400 "Bad Request"
+        401 "Unauthorized"
+        402 "Payment Required"
+        403 "Forbidden"
+        404 "Not Found"
+        405 "Method Not Allowed"
+        406 "Not Acceptable"
+        407 "Proxy Authentication Required"
+        408 "Request Timeout"
+        409 "Conflict"
+        410 "Gone"
+        411 "Length Required"
+        412 "Precondition Failed"
+        500 "Internal Server Error"
+        501 "Not Implemented"
+        502 "Bad Gateway"
+        503 "Service Unavailable"
+        504 "Gateway Timeout"))
+
+(define code->message (hash-ref message-codes _))
+
+(struct exn:fail:network:http:code exn:fail:network (code) #:transparent)
+
+(define (make-http-exn handler-response)
+  (define code (http-response-code handler-response))
+  (define body (http-response-body handler-response))
+  (exn:fail:network:http:code (~a body) (current-continuation-marks) code))
+
+(define (http-exn-of-code? code v)
+  (and (exn:fail:network:http:code? v)
+       (= code (exn:fail:network:http:code-code v))))
+
+(define (raise-request-error handler-response)
+  (raise (make-http-exn handler-response)))
+
+(define failure-code? (<= 400 _ 600))
+
+(define (check-code handler-response)
+  (when (failure-code? (http-response-code handler-response))
+    (raise-request-error handler-response)))
+
+(define (parse-response handler-response)
+  (check-code handler-response)
+  (http-response-body handler-response))
+
+(define requester-http-exn (wrap-requester-response parse-response _))
---
+++ request/private/exn.scrbl
@@ -0,0 +1,33 @@
+#lang scribble/manual
+
+@(require "../doc-utils/examples.rkt"
+          "../doc-utils/def.rkt"
+          (for-label request
+                     racket))
+
+@title{HTTP Status Code Exception Throwing}
+
+@defstruct*[(exn:fail:network:http:code exn:fail:network)
+            ([code exact-positive-integer?])]{
+  An exception structure type for non-success HTTP error codes.
+  All codes in the range @racket[(<= 400 code 599)] are defined
+  as error cases. This exception is thrown by @racket[http-requester]s
+  wrapped by @racket[requester-http-exn].
+}
+
+@defproc[(requester-http-exn [requester requester?])
+         requester?]{
+  Given a @racket[requester] whose responses are @racket[http-response]s,
+  returns a requester whose responses are the only the response bodies
+  of @racket[requester]. In the event of failure error codes
+  in the response, an @racket[exn:fail:network:http:code] exception
+  is thrown which contains the code and response body.
+}
+
+@defproc[(http-exn-of-code? [code exact-positive-integer?]
+                            [v any/c])
+         boolean?]{
+  Returns @racket[#t] if @racket[v] is an instance of
+  @racket[exn:fail:network:http:code] whose status code is
+  @racket[code], and returns @racket[#f] otherwise.
+}
