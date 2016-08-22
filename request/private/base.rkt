72fee36bbcca76f6e6613a6bc50c98e67a1ea074
DarrenN <info@v25media.com>
2016-08-22 06:50:15 -0400

    WIP Add json-requester

--- request/private/base.rkt
+++ request/private/base.rkt
@@ -2,11 +2,18 @@
 
 (require net/url
          fancy-app
+         json
          "call-response.rkt"
          "struct.rkt")
 
-(provide http-requester)
+(provide http-requester
+         json-requester)
 
+(define json-headers
+  '("Accept: application/json" "Content-Type: application/json"))
+
+(define (merge-headers headers)
+  (flatten (cons json-headers headers)))
 
 (define (http-get url #:headers [headers '()])
   (call-response/input-url url (get-impure-port _ headers)))
@@ -20,8 +27,37 @@
 (define (http-delete url #:headers [headers '()])
   (call-response/input-url url (delete-impure-port _ headers)))
 
+(define (http-json-get url #:headers [headers '()])
+  (call-response/input-json-url url (get-impure-port _ headers)))
+
+(define (http-json-put url body #:headers [headers '()])
+  (call-response/input-json-url url (put-impure-port _ body headers)))
+
+(define (http-json-post url body #:headers [headers '()])
+  (call-response/input-json-url url (post-impure-port _ body headers)))
+
+(define (http-json-delete url #:headers [headers '()])
+  (call-response/input-json-url url (delete-impure-port _ headers)))
+
+(define (wrap-json-headers method-fn)
+  (λ (url #:headers [headers '()] . rest)
+    (define json-headers (merge-headers headers))
+    (apply method-fn url #:headers json-headers rest)))
+
+(define (wrap-json-body method-fn)
+  (λ (url body #:headers [headers '()] . rest)
+    (define json-headers (merge-headers headers))
+    (define json-body (jsexpr->bytes body))
+    (apply method-fn url json-body #:headers json-headers rest)))
+
 (define http-requester
   (requester http-get
              http-put
              http-post
              http-delete))
+
+(define json-requester
+  (requester (wrap-json-headers http-json-get)
+             (wrap-json-body http-json-put)
+             (wrap-json-body http-json-post)
+             (wrap-json-headers http-json-delete)))
--- request/private/call-response.rkt
+++ request/private/call-response.rkt
@@ -2,12 +2,14 @@
 
 (require typed/net/url
          typed/net/head
-         fancy-app)
+         fancy-app
+         typed/json)
 
 (provide (struct-out http-response)
          HttpResponse
          Url
-         call-response/input-url)
+         call-response/input-url
+         call-response/input-json-url)
 
 
 (struct http-response
@@ -15,7 +17,13 @@
    [headers : (HashTable String String)]
    [body : String]) #:transparent)
 
+(struct json-response
+  ([code : Positive-Integer]
+   [headers : (HashTable String String)]
+   [body : JSExpr]) #:transparent)
+
 (define-type HttpResponse http-response)
+(define-type JsonResponse json-response)
 (define-type Url url)
 
 (: not-newline? (-> Char Boolean))
@@ -41,17 +49,36 @@
   (define code-chars (takef dropped-protocol not-whitespace?))
   (cast (string->number (apply string code-chars)) Positive-Integer))
 
-(: impure-port->response (-> Input-Port HttpResponse))
-(define (impure-port->response impure-port)
+(: impure-port->headers
+   (-> Input-Port (Values Positive-Integer (HashTable String String))))
+(define (impure-port->headers impure-port)
   (define HTTP-header+MIME-headers (purify-port impure-port))
   (define-values (HTTP-header MIME-headers)
     (split-combined-header HTTP-header+MIME-headers))
   (define status-code (http-header-code HTTP-header))
-  (define headers (cast (make-hash (extract-all-fields MIME-headers)) (HashTable String String)))
+  (define headers
+    (cast (make-hash (extract-all-fields MIME-headers))
+          (HashTable String String)))
+  (values status-code headers))
+
+(: impure-port->http-response (-> Input-Port HttpResponse))
+(define (impure-port->http-response impure-port)
+  (define-values (status-code headers)
+    (impure-port->headers impure-port))
   (define raw-body (port->string impure-port))
   (http-response status-code headers raw-body))
 
+(: impure-port->json-response (-> Input-Port JsonResponse))
+(define (impure-port->json-response impure-port)
+  (define-values (status-code headers)
+    (impure-port->headers impure-port))
+  (define json-body (string->jsexpr (port->string impure-port)))
+  (json-response status-code headers json-body))
 
 (: call-response/input-url (-> Url (-> Url Input-Port) HttpResponse))
 (define (call-response/input-url url connect)
-  (call/input-url url connect impure-port->response))
+  (call/input-url url connect impure-port->http-response))
+
+(: call-response/input-json-url (-> Url (-> Url Input-Port) JsonResponse))
+(define (call-response/input-json-url url connect)
+  (call/input-url url connect impure-port->json-response))
